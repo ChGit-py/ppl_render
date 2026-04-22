ea1f79f8d03ed379d857e93d818ab8fb8e1ffa99
ChGit-py <cegejuru95@live.co.uk>
2026-04-22 19:40:18 +0100

    Modify some of the xG differentials colour scheme

--- fpl_dash_updated.py
+++ fpl_dash_updated.py
@@ -1687,7 +1687,7 @@ app.layout = html.Div([
                             style_data_conditional=[
                                 {'if': {'row_index': 'odd'}, 'backgroundColor': '#fafafa'},
                                 {'if': {'filter_query': '{xg_diff} < -1', 'column_id': 'xg_diff'},
-                                 'backgroundColor': "#F5604E"},
+                                 'backgroundColor': "#FFEBEE"},
                                 {'if': {'filter_query': '{xg_diff} >= -1 && {xg_diff} <= 1', 'column_id': 'xg_diff'},
                                  'backgroundColor': '#FFB938'},
                                 {'if': {'filter_query': '{xg_diff} > 1', 'column_id': 'xg_diff'},
