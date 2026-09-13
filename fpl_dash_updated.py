2c6efe01bab7f28eaa264a689d8dc50e2bab55d5
ChGit-py <cegejuru95@live.co.uk>
2026-09-13 07:49:56 +0100

    Minor visual changes

--- fpl_dash_updated.py
+++ fpl_dash_updated.py
@@ -617,7 +617,7 @@ def summarise_league_chips(snapshots, entries, bootstrap_data, current_gw_num, m
             if played_in_window:
                 row[c] = f"GW{played_in_window[-1]['event']}"
             else:
-                row[c] = 'Held'
+                row[c] = 'held'
                 held_counts[c] += 1
         matrix.append(row)
 
